e4c7e4bede5933201beda94de8c54998a68d485b
AmjustGettingStarted <gamerhmv2050@gmail.com>
2026-09-17 10:11:14 +0530

    feat: add new contact us page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/routes';
 import { KalyaniProvider } from './context/KalyaniContext';
+import { HeaderProvider } from './context/HeaderContext';
 
 export default function App() {
   return (
     <KalyaniProvider>
-      <RouterProvider router={router} />
+      <HeaderProvider>
+        <RouterProvider router={router} />
+      </HeaderProvider>
     </KalyaniProvider>
   );
 }
